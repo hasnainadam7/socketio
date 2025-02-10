38e409508be285de7072de4ab37e8a552301296a
Muhammad Hasnain <79312974+hasnainadam7@users.noreply.github.com>
2025-02-10 18:35:30 +0500

    first commit

---
+++ lib/app/data/helper/dependency.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../data/client_api.dart';
+import '../utils/constants.dart';
+
+Future<void> init() async {
+  // Obtain shared preferences.
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  Get.lazyPut(() => prefs);
+  // Registering ApiClient
+  Get.lazyPut(
+    () => ApiClient(appBaseUrl: Constants.baseUrl),
+  );
+  //
+  // Registering repositories
+  // Get.lazyPut(
+  //         () => AuthRepo(apiClient: Get.find(), sharedPreferences: Get.find()));
+  //
+  // Get.lazyPut(() => CartRepo(sharedPreferences: Get.find()));
+  // Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
+  // Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  // Get.lazyPut(() => UserRepo(apiClient: Get.find()));
+  // Get.lazyPut(
+  //         () => LocationRepo(apiClient: Get.find(), sharedPreferences: Get.find()));
+  // Get.lazyPut(() => PaymentRepo());
+  //
+  // // Registering controllers
+  // Get.lazyPut(() => AuthRepoController(authRepo: Get.find()));
+  // Get.lazyPut(() => ProductRepoController(
+  //     popularProductRepo: Get.find(), recommendedProductRepo: Get.find()));
+  // Get.lazyPut(() => CartRepoController(cartRepo: Get.find()));
+  // Get.lazyPut(() =>
+  //     UserRepoController(userRepo: Get.find(), sharedPreferences: Get.find()));
+  // Get.lazyPut(() => LocationRepoController(locationRepo: Get.find()));
+  // Get.lazyPut(() => PaymentRepoController(paymentRepo: Get.find()));
+}
---
+++ lib/app/data/models/users_models.dart
@@ -0,0 +1,14 @@
+class User {
+  final String name;
+  final String status;
+  final String imagePath;
+  final bool isOnline;
+
+  User({
+    required this.name,
+    required this.status,
+    required this.imagePath,
+    required this.isOnline,
+  });
+}
+
